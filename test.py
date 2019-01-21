dd86ed6190ff9a5ddbac8a921b044f17100666ce
Sergey Zakrytnoy <szakrytnoy@gmail.com>
2019-01-21 23:31:31 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ from input_funcs import prepare_single_input
 from knn_wrapped import knn_classifier
 
 test_input = './data/stroke_6_0011.csv'
-x_train = np.load('x.npy')
-y_train = np.load('y.npy')
+x_train = np.load('./temp/x.npy')
+y_train = np.load('./temp/y.npy')
 
 x_test = prepare_single_input(test_input)
 l = knn_classifier(x_test, 5, x_train, y_train)
